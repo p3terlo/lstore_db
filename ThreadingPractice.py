0a3c1fc5721e6330b5ca9e4d36f647e332282abf
Carolineqyau <54416486+Carolineqyau@users.noreply.github.com>
2021-02-16 04:26:58 +0800

    fixed indentation

--- ThreadingPractice.py
+++ ThreadingPractice.py
@@ -6,26 +6,26 @@ import time
 exitFlag = 0
 
 class myThread (threading.Thread):
-   def __init__(self, threadID, name, q):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.q = q #counter
-   def run(self):
-      print("Starting " + self.name)
-      process_data(self.name, self.q)
-      print("Exiting " + self.name)
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q #counter
+    def run(self):
+        print("Starting " + self.name)
+        process_data(self.name, self.q)
+        print("Exiting " + self.name)
 
-   def process_data(threadName, q):
-      while not exitFlag:
-         queueLock.acquire()
-         #The acquire() method will be called when the block is entered, and release() will be called when the block is exited
-            if not workQueue.empty():
-               data = q.get()
-               queueLock.release()
-               print("%s processing %s" % (threadName, data))
-            else:
-               queueLock.release()
+    def process_data(threadName, q):
+        while not exitFlag:
+            queueLock.acquire()
+            #The acquire() method will be called when the block is entered, and release() will be called when the block is exited
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print("%s processing %s" % (threadName, data))
+        else:
+            queueLock.release()
             time.sleep(1)
 
 threadList = ["Thread-1", "Thread-2", "Thread-3"]
@@ -38,25 +38,25 @@ threadID = 1
 
 # Create new threads
 for tName in threadList:
-   thread = myThread(threadID, tName, workQueue)
-   thread.start()
-   threads.append(thread)
-   threadID += 1
+    thread = myThread(threadID, tName, workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
 
 # Fill the queue
 queueLock.acquire()
 for word in nameList:
-   workQueue.put(word)
+    workQueue.put(word)
 queueLock.release()
 
 # Wait for queue to empty
 while not workQueue.empty():
-   pass
+    pass
 
 # Notify threads it's time to exit
 exitFlag = 1
 
 # Wait for all threads to complete
 for t in threads:
-   t.join()
+    t.join()
 print("Exiting Main Thread")
