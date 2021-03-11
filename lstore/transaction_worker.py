cbea1ead1cfda3ecc30991fab5f89fb67f4e3fc0
Alitorzero <alvinfurre@gmail.com>
2021-03-10 20:39:34 -0800

    ADDED appened to add()

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,5 +1,6 @@
 from lstore.table import Table, Record
 from lstore.index import Index
+from lstore.config import *
 
 class TransactionWorker:
 
@@ -22,24 +23,23 @@ class TransactionWorker:
     """
     def add_transaction(self, t):
         self.transactions.append(t)
+        THREAD_MASTER.append(t)
 
     """
     Runs a transaction
     """
-    def run(self, transaction_queue):
+    def run(self):
         for transaction in self.transactions:
             #push to transaction Queue for planning threads top\pull from
-            transaction_queue.append(transaction)
-
 
 
             # each transaction returns True if committed or False if aborted
-            # self.stats.append(transaction.run())
+            self.stats.append(transaction.run())
         # stores the number of transactions that committed
-
+        self.result = len(list(filter(lambda x: x, self.stats)))
 
     def display_worker(self):
         print("len(transactions)",len(self.transactions))
         for i in range(len(self.transactions)):
-            print(self.transactions[i])
-            print(self.transactions[i].display_transaction())
+            self.transactions[i].print_queries()
+            #print(self.transactions[i].display_transaction())
